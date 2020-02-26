35f7186e8de8d0ba4aa7ec1d888d07c411e9024a
Jiaping Wang <jiapw.cg@gmail.com>
2020-02-26 14:54:43 -0800

    !R: more test on sparsehash

--- testcases/tests/test_ext.cpp
+++ testcases/tests/test_ext.cpp
@@ -3,25 +3,51 @@
 
 void rt::UnitTests::sparsehash()
 {
-	ext::fast_set<int>	set;
-	set.insert(1);
-	set.insert(2);
-	set.insert(3);
-	set.insert(4);
-	set.insert(5);
-	set.insert(6);
-	set.insert(7);
-	set.insert(8);
-	set.insert(9);
-	set.insert(10);
+	{
+		ext::fast_set<int>	set;
+		set.insert(1);
+		set.insert(2);
+		set.insert(3);
+		set.insert(4);
+		set.insert(5);
+		set.insert(6);
+		set.insert(7);
+		set.insert(8);
+		set.insert(9);
+		set.insert(10);
 
-	for(auto it: set)
-		if((it & 1) == 0)
-			set.erase(it);
+		for(auto it = set.begin(); it != set.end(); it++)
+			if((*it & 1) == 0)
+				set.erase(it);
 
-	rt::String out;
-	for(auto it: set)
-		out += rt::SS(" ") + it;
+		rt::String out;
+		for(auto it: set)
+			out += rt::SS(" ") + it;
 
-	_LOG(out);
+		_LOG(out);
+	}
+
+	{
+		ext::fast_map<int, LPCSTR>	set;
+		set.insert(std::make_pair(1,"1"));
+		set.insert(std::make_pair(2,"2"));
+		set.insert(std::make_pair(3,"3"));
+		set.insert(std::make_pair(4,"4"));
+		set.insert(std::make_pair(5,"5"));
+		set.insert(std::make_pair(6,"6"));
+		set.insert(std::make_pair(7,"7"));
+		set.insert(std::make_pair(8,"8"));
+		set.insert(std::make_pair(9,"9"));
+		set.insert(std::make_pair(10, "10"));
+
+		for(auto it = set.begin(); it != set.end(); it++)
+			if((it->first & 1) == 1)
+				set.erase(it);
+
+		rt::String out;
+		for(auto it: set)
+			out += rt::SS(" ") + it.second;
+
+		_LOG(out);
+	}
 }
