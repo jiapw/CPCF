22ff37c550e30b1eec5aa6fce37ac6b9bcd14d6a
ZHANG XIAOBING <zhangxbng@gmail.com>
2020-10-15 14:33:03 +0800

    !R: first build

--- testcases/proj.andriod/testing/jni/Android.mk
+++ testcases/proj.andriod/testing/jni/Android.mk
@@ -5,19 +5,29 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := shared_testing
 LOCAL_SRC_FILES :=	\
 ../../../../core/rt/xml_xhtml.cpp \
+../../../../core/rt/json.cpp \
 ../../../../core/os/kernel.cpp \
+../../../../core/os/kernal_encode.cpp \
+../../../../core/os/kernel_log.cpp \
+../../../../core/os/high_level.cpp \
 ../../../../core/os/file_zip.cpp \
 ../../../../core/os/file_dir.cpp \
 ../../../../core/os/multi_thread.cpp \
+../../../../core/os/precompiler.cpp \
 ../../../../core/inet/inet.cpp \
+../../../../core/inet/http_client.cpp \
 ../../../../core/inet/tinyhttpd.cpp \
 ../../../../core/inet/tinyhttpd_fileserv.cpp \
 ../../../../core/ext/botan/botan.cpp \
 ../../../../core/ext/lib_inc.c \
+../../../../core/ext/exprtk/exprtk.cpp \
+../../../../core/ext/rocksdb\rocksdb.cpp \
 ../../../../testcases/tests/entrypoint.cpp \
 ../../../../testcases/tests/test_api.cpp \
+../../../../testcases/tests/test_ext.cpp \
 ../../../../testcases/tests/test_http.cpp \
 ../../../../testcases/tests/test_botan.cpp \
+../../../../testcases/tests/test_perf.cpp \
 test.c \
 
 LOCAL_LDLIBS    := -lm -llog
@@ -31,7 +41,8 @@ APP_OPTIM := debug
 LOCAL_CFLAGS := -D_DEBUG
 LOCAL_CFLAGS += -D_GLIBCXX_PERMIT_BACKWARD_HASH
 LOCAL_CFLAGS += -DPLATFORM_ANDROID
-
+LOCAL_CFLAGS += -DPNG_ARM_NEON_OPT=0
+LOCAL_CFLAGS += -DOS_ANDROID
 
 
 include $(BUILD_SHARED_LIBRARY)
